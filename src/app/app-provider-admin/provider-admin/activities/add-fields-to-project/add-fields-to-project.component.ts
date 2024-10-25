2a2e655ea91f599b7ce0765fe712061cab01862b
devikasuresh20 <57424483+devikasuresh20@users.noreply.github.com>
2024-10-25 14:56:25 +0530

    AMM-1036 Project configuration changes (#39)
    * Service line filteration changes in institution screens
    
    * Changes in Item Master screen
    
    * Main store-sub store and project related changes
    
    * procedure component mapping changes
    
    * Project configuration changes

--- src/app/app-provider-admin/provider-admin/activities/add-fields-to-project/add-fields-to-project.component.ts
+++ src/app/app-provider-admin/provider-admin/activities/add-fields-to-project/add-fields-to-project.component.ts
@@ -25,6 +25,7 @@ export class AddFieldsToProjectComponent implements OnInit {
   enableUpdate = false;
   setMin: any;
   setMax: any;
+  projectId: any;
 
   fieldTypesList: any = [];
 
@@ -57,7 +58,8 @@ export class AddFieldsToProjectComponent implements OnInit {
 
   ngOnInit() {
     console.log('data', this.input);
-    this.dialogData = this.input;
+    this.dialogData = this.input?.data;
+    this.projectId = this.input?.projectId;
     this.fetchAddedFields();
     this.getAllSectionsData();
   }
@@ -86,6 +88,7 @@ export class AddFieldsToProjectComponent implements OnInit {
     const reqObj = {
       sectionId: this.dialogData.sectionId,
       serviceProviderId: sessionStorage.getItem('service_providerID'),
+      projectId: this.projectId,
     };
     this.addFieldsService.fetchFields(reqObj).subscribe(
       (res: any) => {
@@ -163,6 +166,7 @@ export class AddFieldsToProjectComponent implements OnInit {
       placeholder: item.placeholder,
       fieldType: item.fieldType,
       fieldTypeId: item.fieldTypeId,
+      projectId: this.projectId,
     };
     this.addFieldsService.updateFields(reqObj).subscribe(
       (res: any) => {
@@ -208,6 +212,7 @@ export class AddFieldsToProjectComponent implements OnInit {
       fieldTypeId: this.addFieldsForm.get('fieldTypeId')?.value,
       modifiedBy: sessionStorage.getItem('uname'),
       serviceProviderId: sessionStorage.getItem('service_providerID'),
+      projectId: this.projectId,
     };
     this.addFieldsService.updateFields(reqObj).subscribe(
       (res: any) => {
@@ -298,6 +303,7 @@ export class AddFieldsToProjectComponent implements OnInit {
       serviceProviderId: sessionStorage.getItem('service_providerID'),
       createdBy: sessionStorage.getItem('uname'),
       fields: [form],
+      projectId: this.projectId,
     };
     console.log('reqObj', reqObj);
     this.addFieldsService.saveFields(reqObj).subscribe(
@@ -406,6 +412,7 @@ export class AddFieldsToProjectComponent implements OnInit {
       const reqObj = {
         sectionId: item,
         serviceProviderId: sessionStorage.getItem('service_providerID'),
+        projectId: this.projectId,
       };
       this.addFieldsService.fetchFields(reqObj).subscribe(
         (res: any) => {
